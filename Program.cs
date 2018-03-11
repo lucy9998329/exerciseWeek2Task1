9b51c0b1efe7a9e2fb8cb2cd5af7a82a6cda4400
Luciany Laurentino <lucylaurentino@gmail.com>
2018-03-11 14:32:27 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exerciseWeek2Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start the program with Clear();
+            Console.Clear();
+            
+            Console.WriteLine("hello");
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            
+        }
+    }
+}
