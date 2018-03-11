b8981d47575010ff2c95bc4548425d1236f2527e
Luciany Laurentino <lucylaurentino@gmail.com>
2018-03-11 22:54:24 +1300

    more commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace exerciseWeek2Task1
 {
+    class Maths
+    {
+        /*static int MultiplyNums()
+        {
+            int product = 4 * 8;
+            return product;
+        }*/
+
+        static int MultiplyNums(int num1, int num2) // parameter can only be used inside the function
+        {
+            int product = num1 * num2;
+            return product;
+        }  
+
+    }
     class Program
     {
+        
         static void Main(string[] args)
-        {
-            //Start the program with Clear();
-            Console.Clear();
-            
-            Console.WriteLine("hello");
-            
-            //End the program with blank line and instructions
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine("Press <Enter> to quit the program");
-            Console.ReadKey();
-            
+       /*{                     
+            //Taske 1a - Write a MultiplyNums Static method and also call it from the Main Method.
+            int answer = MultiplyNums();
+            Console.WriteLine(answer);            
+        }*/  
+        
+
+
+        {                   
+            //Taske 1b - Write the same exercise and use paramaters in the method. 
+            //int answer = MultiplyNums(4, 8); //call the function by passing it a string value with the parameter name 
+            //Console.WriteLine(answer);            
+        }  
+        
+            //Task 1c -Create a class called Maths and move the method MultiplyNums into that class.
+            //Call the method from the Main() method.                   
         }
+                                                             
     }
-}
+
